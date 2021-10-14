071b4f94e2a28f31084485452e0639219ee126ec
Brian <brianbawden001@gmail.comgit config --global user.name Brianrungit config --global user.email brianbawden001@gmail.com>
2021-10-14 08:30:56 -0400

    added user_guess to guesser.py

--- jumper/game/guesser.py
+++ jumper/game/guesser.py
@@ -3,3 +3,11 @@ class Guesser():
     def __init__(self):
 
         pass
+
+    def user_guess(self):
+        '''Ask the user to guess a letter that the director will send to the console to print.
+        
+        Returns text to prompt user to guess a letter.
+        '''
+        guess = "Guess a letter: "
+        return guess

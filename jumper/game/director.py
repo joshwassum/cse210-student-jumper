a2c10285916257f92a7f2844d18d45edbf165d96
Larry <copell@hotmail.com>
2021-10-15 09:50:26 -0600

    added director init, get_inputs

--- jumper/game/director.py
+++ jumper/game/director.py
@@ -17,8 +17,19 @@ class Director():
 
         self.keep_playing (boolean): Checks to see if the game continues
 
-        self.guesser (Guesser()): An instance of the class of objects known as Seeker
+        self.guesser (Guesser()): An instance of the class of objects known as Guesser
     '''
+    def __init__(self):
+        """The class constructor.
+        
+        Args:
+            self (Director): an instance of Director.
+        """
+        self.console = Console()
+        self.parachute = Parachute()
+        self.keep_playing = True
+        self.guesser = Guesser()
+        self.letter = ""
 
     def start_game(self):
         """Starts the game loop to control the sequence of play.
@@ -32,10 +43,32 @@ class Director():
             self.do_updates()
             self.do_outputs()
 
+    def do_updates(self):
+        """gets the updates for the game.
+        Args:
+            self (Director): an instance of Director.
+        """
+
+        self.guesser.get_secret_word()
+        #self.do_outputs()
+
+
     def do_outputs(self):
         '''Outputs the important game information for each round of play.
         '''
-        reveal = self.guesser.reveal_letter(self.get_inputs)
-        self.console.write(reveal)
+        if self.guesser.letter_check(self.letter) == True:
+            reveal = self.guesser.reveal_letter(self.letter)
+            self.console.write(reveal)
         if self.parachute.parachute[-1] == "x":
             self.keep_playing = False   
+
+    def get_inputs(self):
+        """Gets the inputs at the beginning of each round of play. 
+
+        Args:
+            self (Director): An instance of Director.
+        """
+        self.console.write(self.parachute.get_parachute())
+        self.console.write(self.guesser.get_secret_word())
+        self.letter = self.console.read(self.guesser.user_guess())
+        
